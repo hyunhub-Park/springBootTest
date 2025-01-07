6e29cfe5a7ecd744f54987b654f747df14068722
hyunhub-park <nuyhos_ambi@naver.com>
2025-01-07 09:34:05 +0900

    [create]Spring Boot 실습.

---
+++ DevProject/src/main/java/com/kh/domain/Board.java
@@ -0,0 +1,33 @@
+package com.kh.domain;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Data
+//@AllArgsConstructor	// 모든 매개변수를 받을 수 있는 생성자.
+//@Getter
+//@Setter
+//@NoArgsConstructor	// dafault.
+//@RequiredArgsConstructor
+//@ToString
+//@EqualsAndHashCode(of="boardNo")
+public class Board 
+{
+	private int boardNo;
+	@NonNull	// title = not null. ex)Board(String)
+	private String title;
+	private String content;
+	private String writer;
+	private Date regDate;
+
+}
